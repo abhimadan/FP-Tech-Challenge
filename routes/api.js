493f986b226034d6bc52d3c13ba1f9239888afe3
Abhishek Madan <abhi.madan01@gmail.com>
2016-06-03 23:05:49 -0400

    Implemented api endpoints

--- routes/api.js
+++ routes/api.js
@@ -4,15 +4,13 @@
 var express     = require('express');
 var https       = require('https');
 var q           = require('q');
+var querystring = require('querystring');
 var api         = express.Router();
 var db          = require('../config/db').connection;
 
 // API endpoint for /api/apparel
 api.get('/api/apparel/:styleCode?', function(req, res) {
-	// Insert Apparel API code here
-
     if (req.params.styleCode) {
-        console.log(req.params.styleCode);
         db.query("SELECT * FROM main_db.apparel WHERE style_code = ?", [req.params.styleCode], function(err, rows, fields) {
             // TODO: return some JSON for the angular controller to parse and display stuff; they give colour codes so maybe use those somehow?
             res.send(rows[0]);
@@ -27,30 +25,63 @@ api.get('/api/apparel/:styleCode?', function(req, res) {
 
 // API endpoint for /api/quote
 api.post('/api/quote', function(req, res) {
-	// Insert Quoting API code here
-
-    // TODO: take style code, colour, size, number of apparel as request data
+    // TODO: take style code, colour, size, weight, number of apparel as request data
     // fetch price
-    // apply logic to add markup, bulk discounts, etc
-    // return object containing all intermediary steps and final answer (as well as individual price and total price)
+    var price = getApparelPrice(req.body.styleCode, req.body.colorCode, req.body.sizeCode)
+        .then(function(value) {
+            var price = value.price;
+            var quantity = req.body.quantity;
+
+            var lqShippingCost, hqShippingCost;
+            if (req.body.weight <= 0.4) {
+                lqShippingCost = 1.00;
+                hqShippingCost = 0.75;
+            } else {
+                lqShippingCost = 0.50;
+                hqShippingCost = 0.25;
+            }
+            var shippingPrice = price + (quantity < 48 ? lqShippingCost : hqShippingCost);
+
+            var salesmanCompPrice = shippingPrice * 1.07;
+
+            var markupPrice = salesmanCompPrice * ((salesmanCompPrice * quantity) <= 800 ? 1.5 : 1.45);
+
+            var totalPrice = markupPrice * quantity;
+
+            res.send({
+                basePrice: price.toFixed(2),
+                shippingPrice: shippingPrice.toFixed(2),
+                salesmanCompPrice: salesmanCompPrice.toFixed(2),
+                markupPrice: markupPrice.toFixed(2),
+                totalPrice: totalPrice.toFixed(2)
+            });
+        });
 });
 
 // Function for making an Inventory API call
 var getApparelPrice = function getPrice(style_code, color_code, size_code) {
 	var	apparelPriceDeferred = q.defer();
 
-	// Format the Inventory API endpoint as explained in the documentation
-	https.get('https://www.alphashirt.com/cgi-bin/online/xml/inv-request.w?in1=B11107196&pr=y&zp=20176&userName=triggered1111&password=triggered2222', function(res) {
-		res.on('data', function (data) {
-			// Parse response XML data here
+    var url = 'https://www.alphashirt.com/cgi-bin/online/xml/inv-request.w';
+    var query = {
+        in1: style_code + color_code + size_code,
+        pr: 'y',
+        zp: '10002',
+        username: 'triggered1111',
+        password: 'triggered2222'
+    };
+    url += '?' + querystring.stringify(query);
 
-            // TODO: parse, replace endpoint to take dynamic parameters
-            apparelPriceDeferred.resolve(data);
+	// Format the Inventory API endpoint as explained in the documentation
+	https.get(url, function(res) {
+        res.setEncoding('utf8');
+		res.on('data', function (data) {
+            var parsedPrice = data.match(/\sprice=\"\$\d+\.\d{2}\"/)[0];
+            var price = parseFloat(parsedPrice.substring(9, parsedPrice.length-1));
+
+            apparelPriceDeferred.resolve({price: price});
 		});
 	}).on('error', function(error) {
-		// Handle EDI call errors here
-
-        // TODO: error handling
         apparelPriceDeferred.reject(error);
 	});
 
